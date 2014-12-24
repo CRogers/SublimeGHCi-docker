dc1f1727cd7fbb1c40db1429b38525985b2377e1
Callum Rogers <callumrogers@hotmail.co.uk>
2014-12-24 00:26:00 +0000

    Add logging to build

--- start_xvfb.sh
+++ start_xvfb.sh
@@ -6,15 +6,17 @@ function sleeper() {
 	sleep $ACTUAL_SLEEP
 }
 
+${LOG_DIR:="/tmp"}
+
 echo Starting xvfb...
-Xvfb -screen 0 1024x768x24 &>>/dev/null &
+Xvfb -screen 0 1024x768x24 >$LOG_DIR/xvfb.stdout 2>$LOG_DIR/xvfb.stderr &
 sleeper
 
 echo Starting ratpoison...
-ratpoison &>>/dev/null &
+ratpoison >$LOG_DIR/ratpoison.stdout 2>$LOG_DIR/ratpoison.stderr &
 
 echo Starting x11vnc...
-x11vnc -display :0 &>>/dev/null &
+x11vnc -display :0 >$LOG_DIR/x11vnc.stdout 2>$LOG_DIR/x11vnc.stderr &
 
 sleeper
 
